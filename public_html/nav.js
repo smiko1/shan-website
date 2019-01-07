286f960fc4aec43f7f4aeba4a8c274918bfd725d
Shannon Miko <smiko@ups.com>
2019-01-07 14:14:26 -0500

    Added footer to pages

--- public_html/nav.js
+++ public_html/nav.js
@@ -11,10 +11,12 @@ function printFooter() {
     $.getScript('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css', function ()
     {
         document.getElementById("myFooter").innerHTML =
-	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
-	+ " rights reserved.</p>"
-	+ "<p id='credits'>Layout by You</p>"
-	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
-	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
+	"<a href='http://www.instagram.com'><i class='fa fa-instagram'></i></a>\
+                <a href='http://www.facebook.com'><i class='fa fa-facebook-official'></i></a>\
+                <a href='http://www.snapchat.com'><i class='fa fa-snapchat'></i></a>\
+                <a href='http://www.pinterest.com'><i class='fa fa-pinterest-p'></i></a>\
+                <a href='http://www.linkedin.com'><i class='fa fa-linkedin'></i></a>\
+                <br>\
+                <a href='/email' target='_blank'>Email Me!</a>";
     });
 }
