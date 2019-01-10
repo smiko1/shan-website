8f29bc0b2eecbedd7100ed96c7a96145ca7e0423
Shannon Miko <smiko@ups.com>
2019-01-10 16:27:33 -0500

    Added info to pages (recipes, bulking info, links to social media, etc.)

--- public_html/nav.js
+++ public_html/nav.js
@@ -5,19 +5,20 @@ function printNavBar() {
                 <a href="recipes.html" class="nav-bar-item">Recipes!</a>\
                 <a href="coaching.html" class="nav-bar-item">Online Coaching & Nutrition</a>\
                 <a href="bulking.html" class="nav-bar-item">Bulking</a>\
-                <a href="cutting.html" class="nav-bar-item">Cutting</a>';
+                <a href="cutting.html" class="nav-bar-item">Cutting</a>\
+                <a href="workout-vids.html" class="nav-bar-item">Workout Vids :)</a>';
                 
     document.getElementById('nav').innerHTML = navigation;
 }
 
 function printFooter() {
-    var footer = "<a href='http://www.instagram.com'><i class='fa fa-instagram'></i></a>\
-                <a href='http://www.facebook.com'><i class='fa fa-facebook-official'></i></a>\
-                <a href='http://www.snapchat.com'><i class='fa fa-snapchat'></i></a>\
-                <a href='http://www.pinterest.com'><i class='fa fa-pinterest-p'></i></a>\
-                <a href='http://www.linkedin.com'><i class='fa fa-linkedin'></i></a>\
+    var footer = "<a href='http://www.instagram.com/shanabanana_x'><i class='fa fa-instagram'></i></a>\
+                <a href='http://www.facebook.com/shan.miko'><i class='fa fa-facebook-official'></i></a>\
+                <a href='http://www.snapchat.com/add/shan.miko'><i class='fa fa-snapchat'></i></a>\
+                <a href='http://www.pinterest.com/shanmiko'><i class='fa fa-pinterest-p'></i></a>\
+                <a href='http://www.linkedin.com/in/shannonmiko'><i class='fa fa-linkedin'></i></a>\
                 <br>\
-                <a href='/email' target='_blank'>Email Me!</a>";
+                <a href='mailto:liftwithshan@gmail.com'>Email Me!</a>";
     document.getElementById("myFooter").innerHTML = footer;
 
 }
