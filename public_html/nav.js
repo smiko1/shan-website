bf88d0ac5726bbfcee2e5d9db6bbcd68844e93cd
Shannon Miko <smiko@ups.com>
2019-01-14 15:56:41 -0500

    Added workout video galleries for each muscle group

--- public_html/nav.js
+++ public_html/nav.js
@@ -11,6 +11,18 @@ function printNavBar() {
     document.getElementById('nav').innerHTML = navigation;
 }
 
+function printNavBarInner() {
+    var navigation = '<div align="center"> <a href="../index.html" class="nav-bar-item">Home</a>\
+                <a href="../training.html" class="nav-bar-item">Personal Training</a>\
+                <a href="../recipes.html" class="nav-bar-item">Recipes!</a>\
+                <a href="../coaching.html" class="nav-bar-item">Online Coaching & Nutrition</a>\
+                <a href="../bulking.html" class="nav-bar-item">Bulking</a>\
+                <a href="../cutting.html" class="nav-bar-item">Cutting</a>\
+                <a href="../workout-vids.html" class="nav-bar-item">Workout Vids :)</a></div>';
+                
+    document.getElementById('nav').innerHTML = navigation;
+}
+
 function printFooter() {
     var footer = "<a href='http://www.instagram.com/shanabanana_x'><i class='fa fa-instagram'></i></a>\
                 <a href='http://www.facebook.com/shan.miko'><i class='fa fa-facebook-official'></i></a>\
